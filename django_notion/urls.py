3bc16043d4cc3e93ec16e3a455b40cde8f38c712
ancient-clever <ancient-clever@outlook.com>
2019-07-23 20:43:46 +0300

    index url changed

--- django_notion/urls.py
+++ django_notion/urls.py
@@ -25,7 +25,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('', home_page, name='index'),
     path('blog', blog_home, name='blog_home'),
     path('blog/category/<int:code>', blog_category, name='blog_category'),
